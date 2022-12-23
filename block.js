83d768500be2aff945689d7087cd9318a8205469
Sandro Simperl <sandrosimperl.cc@gmail.com>
2022-12-23 09:13:48 +0000

    Block 1

---
+++ block.js
@@ -0,0 +1,24 @@
+
+// Klasse Block erstellen
+class Block {
+    // Konstruktor erstellen
+    constructor( timestamp, lastHash, hash, data ){
+        this.timestamp = timestamp;         // Zeitstempel
+        this.lastHash = lastHash;           // Hash des vorhergehenden Blocks
+        this.hash = hash;                   // Eigener Hash
+        this.data = data;                   // Daten
+    }
+
+    // für Debugging Zwecke alles als String Ausgabe
+    toString() { 
+        return `
+            Block -
+                Timestamp:  ${this.timestamp}
+                Last Hash:  ${this.lastHash}
+                Hash:       ${this.hash}
+                Data:       ${this.data}
+        `
+    }
+}
+module.exports = Block;
+//export default Block;
