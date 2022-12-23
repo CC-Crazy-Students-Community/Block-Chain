f91c1447fcf4bce6efa0013a808b763f5ded4314
Sandro Simperl <sandrosimperl.cc@gmail.com>
2022-12-23 10:02:29 +0000

    2 blöcke

--- block.js
+++ block.js
@@ -12,13 +12,38 @@ class Block {
     // für Debugging Zwecke alles als String Ausgabe
     toString() { 
         return `
-            Block 1 -
+            Block -
                 Timestamp:  ${this.timestamp}
                 Last Hash:  ${this.lastHash}
                 Hash:       ${this.hash}
                 Data:       ${this.data}
         `
     }
+
+    // genesis erster Block, intern static auf this schauend
+    static genesis() {
+        return new this( 
+            "Genesis Time",
+            "ZeroHash",
+            "HashBlock0",
+            ["Genesis Data", "data1", "data2"]
+        )
+    }
+
+    // zweiter Block in der Chain
+        static mineBlock1( lastBlock, data ) {
+            const timestamp = Date.now();                       // Zeitstempel in ms erzeugen
+            const lastHash = lastBlock.hash;                    // übergabe des letzten Hashwertes
+            const hash = "HashBlock1"                           // erzeuge neuen Hashwert
+            return new this( timestamp, lastHash, hash, data ); // erzeuge Ausgabe
+        }
+
+    // dritter Block in der Chain
+        static mineBlock2( lastBlock, data ) {
+            const timestamp = Date.now();                       // Zeitstempel in ms erzeugen
+            const lastHash = lastBlock.hash;                    // übergabe des letzten Hashwertes
+            const hash = "HashBlock2"                           // erzeuge neuen Hashwert
+            return new this( timestamp, lastHash, hash, data ); // erzeuge Ausgabe
+        }
 }
 module.exports = Block;
-//export default Block;
--- dev-test.js
+++ dev-test.js
@@ -3,12 +3,24 @@
 // Import Block
 const Block = require("./block");
 
-// Test dre Block Funktionalität
-const block = new Block(
-    "aktuelle Zeit",
-    "Hash 1",
-    "Hash 2",
-    "meine Daten"
-);
+// Test der Block Funktionalität mit neuem Objekt
+    const block = new Block(
+        "aktuelle Zeit",
+        "Hash 1",
+        "Hash 2",
+        "meine Daten"
+    );
+    // console.log( block.toString() );
 
-console.log( block.toString() );
+// Test der Block Funktionalität von der Klasse aus mit statischer Methode
+    //console.log( Block.genesis().toString() );
+    // Genesis Block
+        const genesisBlock = Block.genesis();
+    // erster Block mit Übergabe
+        const testBlock1 = Block.mineBlock1( genesisBlock, "Block1Text" );
+    // zweiter Block mit Übergabe
+        const testBlock2 = Block.mineBlock2( testBlock1, "Block2Text" );
+
+    console.log( genesisBlock.toString() );
+    console.log( testBlock1.toString() );
+    console.log( testBlock2.toString() );
